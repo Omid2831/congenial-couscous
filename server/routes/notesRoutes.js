437e19180532ec812ad3dbda225c156c82732783
Omid2831 <mehrabiomid8282@gmail.com>
2025-11-03 20:32:53 +0100

    fix: implement notes API routes for CRUD operations

---
+++ server/routes/notesRoutes.js
@@ -0,0 +1,39 @@
+import express from 'express';
+
+const router = express.Router();
+
+
+router.get('/api/notes', (req, res) => {
+    // send the notes
+    res.status(200).send('you just fetched the notes');
+});
+
+// Define a route to create notes
+router.post('/api/notes', (req, res) => {
+    // creating a note as a message to the user visually
+    res.status(201).json({
+        message: "post created successfully!"
+    })
+});
+
+// update a route by its id
+router.put('/api/notes/:id', (req, res) => {
+    // creating a note as a message to the user visually
+    res.status(200).json({
+        message: "post updated successfully!"
+    })
+});
+
+// delete a route by its id
+app.delete('/api/notes/:id', (req, res) => {
+    // creating a note as a message to the user visually
+    res.status(200).json({
+        message: "post deleted successfully!"
+    })
+});
+
+
+export default router;
+
+
+
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+import noteRoute from './routes/noteRoutes.js';
+
 // Import Express framework
 const express = require('express');
 
@@ -7,35 +9,7 @@ const app = express();
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
-// Define a GET route to fetch notes
-app.get('/api/notes', (req, res) => {
-    // send the notes
-    res.status(200).send('you got the file here');
-});
 
-// Define a route to create notes
-app.post('/api/notes', (req, res) => {
-    // creating a note as a message to the user visually
-    res.status(201).json({
-        message: "post created successfully!"
-    })
-});
-
-// update a route by its id
-app.put('/api/notes/:id', (req, res) => {
-    // creating a note as a message to the user visually
-    res.status(200).json({
-        message: "post updated successfully!"
-    })
-});
-
-// delete a route by its id
-app.delete('/api/notes/:id', (req, res) => {
-    // creating a note as a message to the user visually
-    res.status(200).json({
-        message: "post deleted successfully!"
-    })
-});
 
 
 // Define the port the server will listen on
