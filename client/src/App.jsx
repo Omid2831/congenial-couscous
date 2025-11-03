0a39e1c73b13f1e59a0a4025406db42d6aed89f0
Omid2831 <mehrabiomid8282@gmail.com>
2025-11-03 19:06:04 +0100

    feat:  setting up the frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-5xl text-center text-gray-500 m-4 p-2'> here we go again </h1>
+    </div>
+  )
+}
+
+export default App
