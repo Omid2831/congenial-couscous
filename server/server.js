a05518ade0b7a011fbe0fc4d43568cb0269c0320
Omid2831 <mehrabiomid8282@gmail.com>
2025-11-03 20:23:59 +0100

    fix: update comments for clarity in notes API routes

--- server/server.js
+++ server/server.js
@@ -12,13 +12,23 @@ app.get('/api/notes', (req, res) => {
     // send the notes
     res.status(200).send('you got the file here');
 });
-// Define a route to delete notes
+
+// Define a route to create notes
 app.post('/api/notes', (req, res) => {
     // creating a note as a message to the user visually
     res.status(201).json({
         message: "post created successfully!"
     })
 });
+
+// update a route by its id
+app.post('/api/notes/:id', (req, res) => {
+    // creating a note as a message to the user visually
+    res.status(200).json({
+        message: "post updated successfully!"
+    })
+});
+
 
 // Define the port the server will listen on
 const port = 8080;
