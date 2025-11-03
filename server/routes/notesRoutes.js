af8a02d38d440928bf02a86cce2c41e2f72ae6af
Omid2831 <mehrabiomid8282@gmail.com>
2025-11-03 21:13:06 +0100

    refactor: remove unnecessary comments from notes routes

--- server/routes/notesRoutes.js
+++ server/routes/notesRoutes.js
@@ -4,14 +4,8 @@ const router = express.Router();
 
 
 router.get('/', getAllNotes);
-
-// Define a route to create notes
 router.post('/post', createNotes);
-
-// update a route by its id
 router.put('/:id', updateNotes);
-
-// delete a route by its id
 router.delete('/:id', deleteNotes);
 
 
