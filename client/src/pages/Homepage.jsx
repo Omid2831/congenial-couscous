33978034c8f953a8a41016adf10f244d9fee32b2
Omid2831 <mehrabiomid8282@gmail.com>
2025-11-04 12:29:03 +0100

    feat: implement data fetching and rate limit handling in Homepage component

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -3,13 +3,37 @@ import toast from 'react-hot-toast'
 import NavBar from '../components/NavBar'
 import { useState } from 'react'
 import RateLimitUI from '../components/RateLimitUI'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 const Homepage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData(URL) {
+      try {
+        const res = await axios.get(URL);
+        console.log(res.data);
+        setNotes(res.data);
+        setIsRateLimited(false)
+      } catch (error) {
+        console.error('Error fetching data');
+        if(error.response.status === 429){
+          setIsRateLimited(true);
+        }else{
+          toast.error('Failed to fetch notes')
+        }
+      }
+    }
+    setURL = 'http://localhost:5173'
+    fetchData(setURL);
+  }, [])
   return (
     <div className='min-h-screen border-4 border-gray-400 border-dashed'>
       <NavBar />
-      
+
       {/* <div className='flex flex-col items-center m-4 gap-4'>
         <h2 className='text-center p-2 m-4 text-5xl'>Homepage</h2>
         <button
