4a0f708ca106791fad5569e1ae8c75dcc9bb4f93
Omid2831 <mehrabiomid8282@gmail.com>
2025-11-04 14:04:14 +0100

    feat: initialize state variables for title, content, and loading in CreateNotes component

--- client/src/pages/CreateNotes.jsx
+++ client/src/pages/CreateNotes.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useState } from 'react'
 
 const CreateNotes = () => {
+  const [[title, setTitle],[content, setContent]] = useState('');
+  const [loading, setLoading] = useState(false);
+
+
+  const handleSubmit = ()=>{};
+  
   return (
     <div>CreateNotes</div>
   )
