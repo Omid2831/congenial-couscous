09a440afb0984f3e76acd3465f7f3e31664609f8
Omid2831 <mehrabiomid8282@gmail.com>
2025-11-04 14:19:23 +0100

    fix: correct state initialization for title and content

--- client/src/pages/CreateNotes.jsx
+++ client/src/pages/CreateNotes.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
-import { useState } from 'react'
+import { ArrowLeft } from 'lucide-react';
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const CreateNotes = () => {
-  const [[title, setTitle],[content, setContent]] = useState('');
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
   const [loading, setLoading] = useState(false);
 
 
-  const handleSubmit = ()=>{};
-  
+  const handleSubmit = () => { };
+
   return (
-    <div>CreateNotes</div>
+    <div className="min-h-screen bg-base-200">
+      <div className="container mx-auto px-4 py-8">
+        <div className="max-w-2xl mx-auto">
+          <Link to={"/"} className="btn btn-ghost mb-6">
+            <ArrowLeft className='size-5' />
+            <span
+              className='text-shadow-amber-900 font-bold text-gray-400 font-mono '>
+              Back to Notes
+            </span>
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
