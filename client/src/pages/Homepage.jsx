7a5067c37eded9145a6f3ef70b32a160ab257ea3
Omid2831 <mehrabiomid8282@gmail.com>
2025-11-04 11:15:22 +0100

    test: the notification system

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import toast from 'react-hot-toast'
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <div>
+      <h2 className='text-center p-2 m-4 text-5xl'>Homepage</h2>
+      <button
+        onClick={() => { toast.success('here we go again') }}>Click Me</button>
+    </div>
   )
 }
 
