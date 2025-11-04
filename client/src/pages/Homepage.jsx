a1d6b1fdb8a771e8e4e955840f8eaaf62b4a61fa
Omid2831 <mehrabiomid8282@gmail.com>
2025-11-04 11:39:24 +0100

    test: enhance Homepage styling with daisy and writing less tailwindcss and optimization!

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -3,10 +3,11 @@ import toast from 'react-hot-toast'
 
 const Homepage = () => {
   return (
-    <div>
+    <div className='flex flex-col items-center m-4 gap-4'>
       <h2 className='text-center p-2 m-4 text-5xl'>Homepage</h2>
       <button
-        onClick={() => { toast.success('here we go again') }}>Click Me</button>
+        onClick={() => { toast.success('here we go again') }}
+        className='btn btn-accent text-center w-45'>Click Me</button>
     </div>
   )
 }
