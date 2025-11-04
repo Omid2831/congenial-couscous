0b45cf3da89e8a2acdf2484c80e8d1ab1d1706f0
Omid2831 <mehrabiomid8282@gmail.com>
2025-11-04 11:07:52 +0100

    feat: giving those components a defined routing for Homepage, Notedetails, and CreateNotes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { Route, Routes } from "react-router"
+import Notedetails from "./pages/Notedetails"
+import Homepage from "./pages/Homepage"
+import CreateNotes from "./pages/CreateNotes"
 
 const App = () => {
   return (
     <div>
-      <h1 className='text-5xl text-center text-gray-500 m-4 p-2'> here we go again </h1>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/noteDetails" element={<Notedetails />} />
+        <Route path="/create" element={<CreateNotes />} />
+      </Routes>
     </div>
   )
 }
